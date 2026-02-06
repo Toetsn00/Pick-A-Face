523b6dfd5fc707e913d6b31f72a12d21b83273db
박은겸 <pek090807@naver.com>
2026-02-06 18:54:07 +0900

    set hydrateRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "./i18n";
 import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById("root")!).render(
+hydrateRoot(
+  document.getElementById("root")!,
   <StrictMode>
     <HelmetProvider>
       <App />
